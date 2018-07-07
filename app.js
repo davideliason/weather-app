5527be68ebf89c780ee6d273ed1d6a0f2566c130
David Eliason <david@davethemaker.com>
2018-07-07 09:47:45 -0700

    refactor code for request functionality to be function

--- app.js
+++ app.js
@@ -18,20 +18,22 @@ console.log("address", argv.a); // address from argv
 var encodedAddress = encodeURIComponent(argv.a);
 console.log(encodedAddress);
 
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log("couldn't connect");
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log("no such address");
-    } else {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    }
+var geolocation = (x) => {
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            console.log("couldn't connect");
+        } else if (body.status === 'ZERO_RESULTS') {
+            console.log("no such address");
+        } else {
+            console.log(`Address: ${body.results[0].formatted_address}`);
+            console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        }
+    })
+};
 
-});
-
+geolocation(encodedAddress);
 
