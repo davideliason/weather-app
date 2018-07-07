fad9348f638eae9167e9f4832508528b3555199e
David Eliason <david@davethemaker.com>
2018-07-07 03:58:35 -0700

    request google api latitude and longitude coordinates and render to console

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1310%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
 
 
